15d745a0aac371fd8c37a92ff9c7caae85cf95d1
Prelang Builder <builder@prelang.com>
2016-01-20 16:53:27 +0000

    Generated scaffolding for model 'Invoice' via Rails

---
+++ app/views/invoices/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @invoice, :id, :contract_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :invoices
+
   resources :payments
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}
